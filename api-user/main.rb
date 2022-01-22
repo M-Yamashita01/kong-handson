889e234345e806affeb34aa7d3f7294ecf67a2ee
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2022-01-22 22:54:44 +0900

    Use plugin to limit access.

--- api-user/main.rb
+++ api-user/main.rb
@@ -2,15 +2,11 @@ require 'kong'
 
 Kong::Client.api_url = 'http://kong:8001'
 
-routes = Kong::Route.all()
-routes.each do |route|
-  route.delete
-end
 
-services = Kong::Service.all()
-services.each do |service|
-  service.delete
-end
+Kong::Plugin.all.each(&:delete)
+Kong::Route.all.each(&:delete)
+Kong::Service.all.each(&:delete)
+Kong::Consumer.all.each(&:delete)
 
 service = Kong::Service.new(
   {
@@ -36,3 +32,47 @@ route = Kong::Route.new(
 )
 
 route.create
+
+consumer = Kong::Consumer.new({ username: 'test-user' })
+consumer.create
+
+cursor = {
+  'config': {
+    minute: 5
+  }
+}
+
+if cursor['config']
+  memo = flatten(cursor.delete('config'), 'config')
+end
+
+plugin = Kong::Plugin.new(
+  {
+    service: {
+      id: service.id
+    },
+    consumer: {
+      id: consumer.id
+    },
+    name: 'rate-limiting',
+    config: {
+      minute: 5
+    }
+  }
+)
+
+plugin.create
+
+
+def flatten(cursor, parent_key = nil, memo = {})
+  memo.tap do
+    case cursor
+    when Hash
+      cursor.keys.each do |key|
+        flatten(cursor[key], [parent_key, key].compact.join('.'), memo)
+      end
+    else
+      memo["#{parent_key}"] = cursor
+    end
+  end
+end
